1fa1b82cd2a30790fd7b95da756c824362806852
miguel-mesa0 <miguelmesa1840@gmail.com>
2025-10-17 16:25:44 -0500

    Actualizacion desde otro equipo

--- estructuras_de_contro_l3147912/Program.cs
+++ estructuras_de_contro_l3147912/Program.cs
@@ -38,19 +38,45 @@ namespace estructuras_de_contro_l3147912
             //    Console.WriteLine("desaprobado");
             //}
 
-            Console.WriteLine("Ingrese el precio del producto:");
-            float product = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Ingrese el precio del producto:");
+            //float product = float.Parse(Console.ReadLine());
 
-            if (product >= 100000)
-            {
-                float discount = product * 0.20f; // 20% de descuento
-                float finalPrice = product - discount;
-                Console.WriteLine($"Se le aplica un 20% de descuento, el precio final es: {finalPrice}");
-            }
-            else
-            {
-                Console.WriteLine($"No se aplica descuento, el precio es: {product}");
-            }
+            //if (product >= 100000)
+            //{
+            //    float discount = product * 0.20f; // 20% de descuento
+            //    float finalPrice = product - discount;
+            //    Console.WriteLine($"Se le aplica un 20% de descuento, el precio final es: {finalPrice}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"No se aplica descuento, el precio es: {product}");
+            //}
+
+            Console.WriteLine("=== COLILLA DE PAGO ===\n");
+
+            // Entradas
+            Console.Write("Ingrese el salario del empleado: ");
+            double salario = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese el valor de ahorro mensual programado: ");
+            double ahorro = Convert.ToDouble(Console.ReadLine());
+
+            // Cálculos
+            double eps = salario * 0.125;
+            double pension = salario * 0.16;
+            double totalDeducciones = eps + pension + ahorro;
+            double totalRecibir = salario - totalDeducciones;
+
+            // Salida
+            Console.WriteLine("\n RESULTADOS ");
+            Console.WriteLine($"Salario del Empleado: ${salario:N2}");
+            Console.WriteLine($"Ahorro Mensual Programado: ${ahorro:N2}");
+            Console.WriteLine($"Deducción EPS (12.5%): ${eps:N2}");
+            Console.WriteLine($"Deducción Pensión (16%): ${pension:N2}");
+            Console.WriteLine($"Total a Recibir: ${totalRecibir:N2}");
+
+            Console.WriteLine("\nPresione cualquier tecla para salir...");
+            Console.ReadKey();
 
 
         }
